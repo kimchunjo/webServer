a3cf25272ddeed9df244d14c0b10719e951de2a2
younhwan97 <78298663+younhwan97@users.noreply.github.com>
2021-04-02 15:30:05 +0900

    web
    version.1

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+/* body parser */
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+/* template engine */
+app.set('view engine', 'pug'); /* jade template engine 을 사용 */
+app.set('views', './pug'); /* template file 은 views 라는 폴더 및에 위치 */
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render('index',{
+        path:'',
+    })
+});
+
+app.get('/login', function (req, res) {
+    res.render('login',{
+        path:'',
+    })
+});
+
+app.get('/signup', function (req, res) {
+    res.render('signup',{
+        path:'',
+    })
+});
+app.listen(8080);
