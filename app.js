ee1099f9d595241b0204dfc7a6e4deb9be82c17e
jscheon <cjswltjr159@naver.com>
2021-04-02 16:14:54 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ app.set('view engine', 'pug'); /* pug template engine 을 사용 */
 app.set('views', './pug'); /* template file 은 pug 라는 폴더 및에 위치 */
 app.use(express.static('public'));
 
+restaurantsJson = JSON.parse(fs.readFileSync('public/js/restaurants-geojson.json', {
+    encoding: 'utf8'
+}));
+roomsJson = JSON.parse(fs.readFileSync('public/js/rooms-geojson.json', {
+    encoding: 'utf8'
+}));
+bookingsJson = JSON.parse(fs.readFileSync('public/js/bookings.json', {
+    encoding: 'utf8'
+}));
+
+
 app.get('/', function (req, res) {
     res.render('index',{
         path:'',
@@ -27,4 +38,19 @@ app.get('/signup', function (req, res) {
         path:'',
     })
 });
-app.listen(8080);
+
+app.get('/category', function(req, res) {
+    res.render('category_custom', {title:'검색결과'});
+});
+
+app.get('/category-map', function(req, res) {
+    res.render('category-map_custom', {title:'지도 검색결과'});
+});
+
+app.get('/index', function(req, res) {
+    res.render('index_custom', {title:'메인'});
+});
+
+app.listen(8080, function(){
+    console.log("실행")
+});
