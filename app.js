4de1a3fae47199bf0a53022abd3719bff2eef72f
jscheon <cjswltjr159@naver.com>
2021-04-10 15:45:59 +0900

    web

--- app.js
+++ app.js
@@ -1,30 +1,111 @@
 var express = require('express');
 var app = express();
+const fs = require('fs');
 
 /* body parser */
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 /* template engine */
-app.set('view engine', 'pug'); /* jade template engine 을 사용 */
-app.set('views', './pug'); /* template file 은 views 라는 폴더 및에 위치 */
+app.set('view engine', 'pug'); /* pug template engine 을 사용 */
+app.set('views', './pug'); /* template file 은 pug 라는 폴더 및에 위치 */
 app.use(express.static('public'));
 
+restaurantsJson = JSON.parse(fs.readFileSync('pug/js/restaurants-geojson.json', {
+    encoding: 'utf8'
+}));
+roomsJson = JSON.parse(fs.readFileSync('pug/js/rooms-geojson.json', {
+    encoding: 'utf8'
+}));
+bookingsJson = JSON.parse(fs.readFileSync('pug/js/bookings.json', {
+    encoding: 'utf8'
+}));
+
+
 app.get('/', function (req, res) {
-    res.render('index',{
-        path:'',
+    res.render('index', {
+        path: '',
     })
 });
 
 app.get('/login', function (req, res) {
-    res.render('login',{
-        path:'',
+    res.render('login', {
+        path: '',
     })
 });
 
 app.get('/signup', function (req, res) {
-    res.render('signup',{
+    res.render('signup', {
+        path: '',
+    })
+});
+
+// 장소 추가
+app.get('/user-add-0', function (req, res) {
+    res.render('user-add-0')
+});
+app.get('/user-add-1', function (req, res) {
+    res.render('user-add-1')
+});
+app.get('/user-add-2', function (req, res) {
+    res.render('user-add-2')
+});
+app.get('/user-add-3', function (req, res) {
+    res.render('user-add-3')
+});
+app.get('/user-add-4', function (req, res) {
+    res.render('user-add-4')
+});
+app.get('/user-add-5', function (req, res) {
+    res.render('user-add-5')
+});
+
+
+
+app.get('/detail', function (req, res) {
+    res.render('detail',{
         path:'',
     })
 });
+
+app.get('/user-account', function (req, res) {
+    res.render('user-account',{
+        path:'',
+    })
+});
+
+app.get('/user-profile', function (req ,res) {
+    let loginId = req.query.loginUsername;
+    let loginPW = req.query.loginPassword;
+    res.render('user-profile',{
+        path:'',
+    })
+});
+
+
+// 검색 결과 페이지
+app.get('/category', function (req, res) {
+    let searchWord = req.query.search;
+    let searchLocation = req.query.location;
+    if (searchWord === undefined || searchWord === "") // 검색어를 입력하변지 않은 경우 사용자 취향을 고려한 검색
+        searchWord = '내 취향에 맞는 장소'
+    if (searchLocation === undefined || searchLocation === "") // 장소를 입력하변지 않은 경우 내 주변으로 검색
+        searchLocation = '근처'
+    res.render('category-custom', {
+        path: '',
+        title: '검색결과',
+        searchWord: searchWord,
+        searchLocation: searchLocation,
+    });
+});
+
+app.get('/category-map', function (req, res) {
+    let searchWord = req.query.search;
+    res.render('category-map-custom', {
+        path: '',
+        title: '지도 검색결과',
+        searchWord: searchWord
+    });
+});
+
 app.listen(8080);
---
+++ public/js/map-custom.js
@@ -0,0 +1,92 @@
+var map // 지도
+var lat // 위도
+var lon // 경도
+var mapContainer    // Container
+var zoomControl     // 줌 컨트롤
+var imageSrc = 'img/marker.svg'; // 마커 이미지의 이미지 주소
+var positions = []  // 장소
+var jsonUrl     // 장소 데이터
+
+// 현재 위치값으로 지도 생성
+navigator.geolocation.getCurrentPosition(
+    function(position){
+
+        lat = position.coords.latitude
+        lon = position.coords.longitude
+
+        mapContainer = document.getElementById('map'), // 지도를 표시할 div
+            mapOption = {
+                center: new kakao.maps.LatLng(lat, lon), // 지도의 중심좌표
+                level: 3 // 지도의 확대 레벨
+            };
+
+        map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+        // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성
+        zoomControl = new kakao.maps.ZoomControl();
+        map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+        // 현재위치 추가
+        var obj = {
+            name: '현재위치',
+            latlng: new kakao.maps.LatLng(lat, lon)
+        }
+        positions.push(obj);
+
+        jsonUrl = 'js/restaurants-geojson.json';    // 데이터 json 파일
+
+        
+
+        $.getJSON(jsonUrl, function (data) {
+            $.each(data, function(key, value){
+                console.log("key: " +key+" : value "+ value)
+            });
+            for(var i = 0; i< data.features.length; i++) {
+                var obj = {
+                    name: data.features[i].properties.name,
+                    latlng: new kakao.maps.LatLng(data.features[i].geometry.coordinates[0], data.features[i].geometry.coordinates[1])
+                }
+                positions.push(obj);
+            }
+
+            makeMaker(map, positions, imageSrc) // 마커 적용
+        })
+
+
+
+    },function (error){
+
+    }
+)
+
+// // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+// function zoomIn() {
+//     map.setLevel(map.getLevel() - 1);
+// }
+//
+// // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+// function zoomOut() {
+//     map.setLevel(map.getLevel() + 1);
+// }
+
+// 마커 만들기
+function makeMaker(map, positions, imageSrc){
+
+    for (var i = 0; i < positions.length; i ++) {
+
+        // 마커 이미지의 이미지 크기 입니다
+        var imageSize = new kakao.maps.Size(24, 35);
+
+        // 마커 이미지를 생성합니다
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: positions[i].latlng, // 마커를 표시할 위치
+            name : positions[i].name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image : markerImage // 마커 이미지
+        });
+    }
+}
+
