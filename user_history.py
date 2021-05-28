2c70bb22caf8e46e5ee59cfef617856180d7f752
younhwan97 <78298663+younhwan97@users.noreply.github.com>
2021-05-28 18:34:52 +0900

    Add files via upload

--- user_history.py
+++ user_history.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[14]:
 
-import json
+
 import sys
 import io
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
@@ -13,21 +13,14 @@ from gensim.models import Word2Vec
 
 def predict(key):
     model = Word2Vec.load("user_history_model")
-    try:
-        return model.most_similar(positive=key)
-    except KeyError:
-        if len(key) == 3:
-            key =[ key[0] , key[1] ]
-            return predict(key)
-        elif len(key) == 2:
-            key =[ key[0] ]
-            return predict(key)
-        elif len(key) == 1:
-            return "not in vocabulary"
+    return model.most_similar(key)
+    
+    
+print(predict(sys.argv[1]))
 
-if __name__ == '__main__':
-    data = []
-    for v in range(1, len(sys.argv)):
-        data.append(json.loads(sys.argv[v])['name'])
-    result = predict(data)
-    print(json.dumps(result))
+
+# In[ ]:
+
+
+
+
