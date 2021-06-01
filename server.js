deb058950912548367efd20895f849b33c94128a
jscheon <cjswltjr159@naver.com>
2021-06-01 18:00:03 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ exports.getToday= function () {
     var week = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
     var today = new Date().getDay();
     var todayLabel = week[today];
-    console.log(11);
     return todayLabel;
 }
 
